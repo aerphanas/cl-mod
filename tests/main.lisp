a9de60c77e99b8fff02dd8d983d790f090bed0d0
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2023-09-28 09:13:48 +0700

    add unit test for cl-mod

--- tests/main.lisp
+++ tests/main.lisp
@@ -3,10 +3,35 @@
 	     :cl
              :cl-mod
              :rove))
+
 (in-package :cl-mod/tests/main)
 
 ;; NOTE: To run this test file, execute `(asdf:test-system :cl-mod)' in your Lisp.
 
-(deftest test-target-1
-  (testing "should (= 1 1) to be true"
-    (ok (= 1 1))))
+(deftest permission-converter-test
+  (testing "rw-r--r-- should be 644"
+	   (ok (string-equal
+		(cl-mod::permission-symbolic-numeric "rw-r--r--")
+		(format nil "644"))))
+  (testing "644 should be rw-r--r--"
+	   (ok (string-equal
+		(cl-mod::permission-numeric-symbolic "644")
+		(format nil "rw-r--r--"))))
+  (testing "rwxrwxrwx should be 777"
+	   (ok (string-equal
+		(cl-mod::permission-symbolic-numeric "rwxrwxrwx")
+		(format nil "777"))))
+  (testing "777 should be rwxrwxrwx"
+	   (ok (string-equal
+		(cl-mod::permission-numeric-symbolic "777")
+		(format nil "rwxrwxrwx")))))
+
+(deftest input-validation-test
+  (testing "input 644 should be valid"
+    (ok (cl-mod::valid-inputp "644" :numeric t)))
+  (testing "input rw-r--r-- should be valid"
+    (ok (cl-mod::valid-inputp "rw-r--r--" :symbolic t)))
+  (testing "input rw-r-r-- should be invalid"
+    (ok (not (cl-mod::valid-inputp "rw-r-r--" :symbolic t))))
+  (testing "input 999 should be invalid"
+    (ok (not (cl-mod::valid-inputp "999" :numeric t)))))
